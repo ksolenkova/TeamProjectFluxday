66df988f6d270a37aae456944783983a4283ef57
Jzph01 <jzph01@gmail.com>
2018-10-10 23:48:35 +0300

    Added LoginProvider

---
+++ TeamProjectFluxday/TeamProjectFluxday/Utils/LoginProvider.cs
@@ -0,0 +1,17 @@
+﻿using TeamProjectFluxday.Data.Models;
+using TeamProjectFluxday.Pages.Dashboard;
+using TeamProjectFluxday.Pages.Login;
+
+namespace TeamProjectFluxday.Utils
+{
+    public static class LoginProvider
+    {
+        public static DashboardPage Login(User user)
+        {
+            var loginPage = new LoginPage();
+            loginPage.Navigate();
+
+            return loginPage.Login(user);
+        }
+    }
+}

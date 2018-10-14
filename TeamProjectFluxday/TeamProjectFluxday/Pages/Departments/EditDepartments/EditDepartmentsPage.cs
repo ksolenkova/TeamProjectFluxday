76dab79369ef232285565d6c605fa5b1fefed102
Kristina Solenkova <krisi909@abv.bg>
2018-10-14 11:03:57 +0300

    Added EditDepartmentsPage Added new test in CreateDepartmentsPageTests Code refactoring

---
+++ TeamProjectFluxday/TeamProjectFluxday/Pages/Departments/EditDepartments/EditDepartmentsPage.cs
@@ -0,0 +1,12 @@
+﻿using TeamProjectFluxday.Core;
+
+namespace TeamProjectFluxday.Pages
+{
+    public class EditDepartmentsPage : BasePage<EditDepartmentsPageMap>
+    {
+        public EditDepartmentsPage(string url) : base(url)
+        {
+        }
+
+    }
+}
---
+++ TeamProjectFluxday/TeamProjectFluxday/Tests/EditDepartmentsPageTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamProjectFluxday.Core;
+
+namespace TeamProjectFluxday.Tests
+{
+    [TestClass]
+    public class EditDepartmentsPageTests : BaseTest
+    {
+        [TestCategory("EditDepartmentsPageTests")]
+        [Owner("Kristina Solenkova")]
+        [TestMethod]
+        public void Test001()
+        {
+
+        }
+    }
+}

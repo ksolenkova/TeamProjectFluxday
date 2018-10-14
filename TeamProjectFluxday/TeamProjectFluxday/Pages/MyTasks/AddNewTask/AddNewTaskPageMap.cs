15f435a386e47e3fa98f7e046c404c10fb490f64
Jzph01 <jzph01@gmail.com>
2018-10-14 15:09:27 +0300

    "+Task" Button validatinon test added.

--- TeamProjectFluxday/TeamProjectFluxday/Pages/MyTasks/AddNewTask/AddNewTaskPageMap.cs
+++ TeamProjectFluxday/TeamProjectFluxday/Pages/MyTasks/AddNewTask/AddNewTaskPageMap.cs
@@ -28,5 +28,13 @@ namespace TeamProjectFluxday.Pages.MyTasks.AddNewTask
                 return GetElement(By.CssSelector(".button"));
             }
         }
+
+        public IWebElement AddNewTaskPageTitle
+        {
+            get
+            {
+                return GetElement(By.XPath(@"//*[@id='new_task']/div[3]/div[1]"));
+            }
+        }
     }
 }
